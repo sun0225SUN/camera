7fcaa8c91fcefd704eb633982eb312ad9dd89080
guoqi.sun <sun412926@gmail.com>
2024-01-24 00:47:07 +0800

    :sparkles: feat: add demo

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <script setup>
+import Data from '@/data/test.json'
+
+const imgList = Data
+// console.log(imgList)
 </script>
 
 <template>
-  <div class="flex flex-col h-[100vh] justify-center items-center">
-    <div
-      text="44px"
-      color="blue"
-    >
-      Unocss
+  <div gap-4 columns-1 lg:columns-4 md:columns-3 sm:columns-2 xl:columns-5>
+    <div v-for="(item, index) in imgList" :key="index">
+      <img rounded-lg w-full :src="item.url">
     </div>
-    <a-button type="primary">
-      Ant-Design-vue
-    </a-button>
-    <div class="flex h-[60px] justify-between items-center">
-      <RouterLink to="/" class="w-[80px]">
-        <div class="text-center">
-          Home
-        </div>
-      </RouterLink>
-      <RouterLink to="/about" class="w-[80px]">
-        <div class="text-center">
-          About
-        </div>
-      </RouterLink>
-    </div>
-
-    <RouterView />
   </div>
 </template>
 
